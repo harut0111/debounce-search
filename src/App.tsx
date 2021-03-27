094f452bcee32b74f248c4cfea6458004f44b442
Harut Muqoyan <hmuqoyan@gmail.com>
2021-03-27 17:35:47 +0400

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,57 @@
 import React from "react";
-import Api from "./api";
+import Api, { resultType } from "./api";
 import "./App.css";
 
+const withDebounce = (
+  callback: (...args: unknown[]) => Promise<void>,
+  wait: number
+) => {
+  let timeoutID: ReturnType<typeof setTimeout>;
+
+  return (...args: unknown[]) => {
+    if (timeoutID) clearTimeout(timeoutID);
+    timeoutID = setTimeout(() => callback(...args), wait);
+  };
+};
+
 const App = (): JSX.Element => {
-  const [result, setResult] = React.useState("Empty");
+  const [result, setResult] = React.useState<resultType>({
+    message: "",
+    status: "",
+  });
 
   const getRandomDogs = async () => {
+    console.log("API Call");
     try {
       const result = await Api.fetchRandomDogs();
-      setResult(JSON.stringify(result, null, "\t"));
+      setResult(result);
     } catch (error) {
       console.log("error", error);
     }
   };
 
-  console.log("result", result);
+  const getRandomDogsWithDebounce = withDebounce(getRandomDogs, 500);
 
   return (
     <div className="App">
+      <div>
+        <b>this case wait time is 500ms...</b>
+        <br />
+        open console to see when api call occured
+        <br />
+        it makes next api call only after 500ms
+      </div>
       <input
         type="text"
         placeholder="type to search..."
-        onChange={getRandomDogs}
+        onChange={getRandomDogsWithDebounce}
       />
-      <div className="result">Result: {result}</div>
+      <div className="result">
+        Result: {result ? JSON.stringify(result) : "Empty"}
+      </div>
+      <div className="image">
+        {result.message && <img width="200" alt="dog" src={result.message} />}
+      </div>
     </div>
   );
 };
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,10 @@
+export interface resultType {
+  message: string;
+  status: string
+}
+
 export default class Api {
-  static fetchRandomDogs = async (): Promise<string> => {
+  static fetchRandomDogs = async (): Promise<resultType> => {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
 
     if (response.ok) return await response.json();
